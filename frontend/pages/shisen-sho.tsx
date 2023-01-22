b18c458e013b0850eba32369a51006c70106c583
lsm <lsm1460@gmail.com>
2023-01-22 21:23:35 +0900

    feat: 사천성 마무리..

--- frontend/pages/shisen-sho.tsx
+++ frontend/pages/shisen-sho.tsx
@@ -1,7 +1,93 @@
 import ShisenShoViewer from '@/src/components/shisenSho/viewer';
+import { useMemo, useState, useEffect } from 'react';
+import _ from 'lodash';
 
 function ShisenSho() {
-  return <ShisenShoViewer />;
+  const [boardSize, setBoardSize] = useState({ x: 5, y: 5 });
+  const [numberOfType, setNumberOfType] = useState<number>(2);
+  const [gameBoard, setGameBoard] = useState<number[][]>(null);
+
+  const cellCount = useMemo(
+    () => ((boardSize.x * boardSize.y) % 2 === 0 ? boardSize.x * boardSize.y : boardSize.x * boardSize.y - 1),
+    [boardSize]
+  );
+
+  useEffect(() => {
+    // 2로 초기화
+    setNumberOfType(2);
+  }, [cellCount]);
+
+  const handleMakeBoard = () => {
+    let _fillCount = cellCount % numberOfType === 0 ? cellCount : cellCount - (cellCount % 2);
+
+    const typeCounts = [];
+
+    for (let i = 0; i < numberOfType; i++) {
+      if (i === numberOfType - 1) {
+        const _last = typeCounts.reduce((acc, cur) => acc + cur, 0);
+        typeCounts.push(_fillCount - _last);
+      } else {
+        const _c = Math.floor(_fillCount / numberOfType);
+        typeCounts.push(_c % 2 === 0 ? _c : _c + 1);
+      }
+    }
+
+    let valueList = typeCounts.map((_count, _i) => new Array(_count).fill(_i + 1)).flat();
+
+    for (let j = 0; j < boardSize.x * boardSize.y - valueList.length; j++) {
+      valueList.push(0);
+    }
+
+    valueList = _.shuffle(valueList);
+
+    const board = new Array(boardSize.y + 2).fill(new Array(boardSize.x + 2).fill(0)).map((line, _i) => {
+      if (_i === 0 || _i === boardSize.y + 1) {
+        return line;
+      } else {
+        return line.map((_v, _i) => (_i === 0 || _i === line.length - 1 ? 0 : valueList.shift()));
+      }
+    });
+
+    setGameBoard(board);
+  };
+
+  return (
+    <div>
+      <p>
+        가로 (3~8)
+        <input
+          max={8}
+          min={3}
+          type="number"
+          value={boardSize.x}
+          onChange={(_event) => setBoardSize((prev) => ({ ...prev, x: parseInt(_event.target.value, 10) }))}
+        />
+      </p>
+      <p>
+        세로 (3~8)
+        <input
+          max={8}
+          min={3}
+          type="number"
+          value={boardSize.y}
+          onChange={(_event) => setBoardSize((prev) => ({ ...prev, y: parseInt(_event.target.value, 10) }))}
+        />
+      </p>
+      <p>{cellCount}</p>
+      <p>
+        {`종류 수 (2~5)`}
+        <input
+          min={2}
+          max={5}
+          type="number"
+          value={numberOfType}
+          onChange={(_event) => setNumberOfType(Math.min(5, Math.max(parseInt(_event.target.value, 10), 2)))}
+        />
+      </p>
+      <button onClick={handleMakeBoard}>만들기</button>
+      {gameBoard && <ShisenShoViewer originBoard={gameBoard} />}
+    </div>
+  );
 }
 
 export default ShisenSho;
--- frontend/src/components/shisenSho/viewer/index.tsx
+++ frontend/src/components/shisenSho/viewer/index.tsx
@@ -1,8 +1,3 @@
-/* TODO: 
-  - 칸의 아이디와 라벨의 분리
-  - 매칭된 칸은 맵에서 제거
-*/
-
 import classNames from 'classnames/bind';
 import styles from './viewer.module.scss';
 const cx = classNames.bind(styles);
@@ -52,17 +47,15 @@ const getAngleCount = (_posList: ItemPos[]) => {
   ).count;
 };
 
-function ShisenShoViewer() {
-  const [gameBoard] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 2, 2, 2, 0, 0, 0],
-    [0, 2, 2, 2, 2, 2, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-  ]);
+interface Props {
+  originBoard: number[][];
+}
+function ShisenShoViewer({ originBoard }: Props) {
+  const [gameBoard, setGameBoard] = useState(originBoard);
   const [path, setPath] = useState<string[]>([]);
-  const [startPos, setStartPos] = useState(null);
-  const [endPos, setEndPos] = useState(null);
+  const [startPos, setStartPos] = useState<ItemPos>(null);
+  const [endPos, setEndPos] = useState<ItemPos>(null);
+  const [shuffleCount, setShuffleCount] = useState(3);
 
   const hashMap: { [key: string]: { pos: ItemPos; closedNode: ItemPos[] } } = useMemo(
     () =>
@@ -94,42 +87,44 @@ function ShisenShoViewer() {
   const findPath = (_start: ItemPos, _end: ItemPos) => {
     const visitedList = [];
     const visited: { [_pathKey: string]: PathInfo } = {},
+      startVal = gameBoard[_start[1]][_start[0]],
       endVal = gameBoard[_end[1]][_end[0]];
     let needVisit: PathInfo[] = [],
       isSuccessFlag = false;
 
-    needVisit.push({ pos: _start, prev: null, prevList: [] });
+    if (startVal === endVal) {
+      needVisit.push({ pos: _start, prev: null, prevList: [] });
 
-    while (needVisit.length > 0) {
-      const path = needVisit.shift();
-      const _pathKey = path.pos.join();
+      while (needVisit.length > 0) {
+        const path = needVisit.shift();
+        const _pathKey = path.pos.join();
 
-      const count = getAngleCount([...path.prevList, path.pos]);
+        const count = getAngleCount([...path.prevList, path.pos]);
 
-      if (
-        gameBoard[path.pos[1]][path.pos[0]] === endVal &&
-        path.pos[1] === _end[1] &&
-        path.pos[0] === _end[0] &&
-        count < 3
-      ) {
-        isSuccessFlag = true;
-        visited[_pathKey] = path;
-        break;
-      }
+        if (path.pos[1] === _end[1] && path.pos[0] === _end[0] && count < 3) {
+          isSuccessFlag = true;
+          visited[_pathKey] = path;
+          break;
+        }
 
-      if (!visitedList.includes(_pathKey) && count < 3) {
-        visited[_pathKey] = path;
-        visitedList.push(_pathKey);
+        if (count < 3 && !visitedList.includes(_pathKey)) {
+          if (!needVisit.map((_need) => _need.pos.join()).includes(_pathKey)) {
+            // 대기열에 남아있을 경우 탐색을 끝내지 않음 (같은 칸 이여도 탐색 히스토리가 다를 수 있음)
+            visited[_pathKey] = path;
+            visitedList.push(_pathKey);
+          }
 
-        const targetPos = hashMap[_pathKey].closedNode
-          .filter((_node) => {
-            const _val = gameBoard[_node[1]][_node[0]];
+          const targetPos = hashMap[_pathKey].closedNode
+            .filter((_node) => {
+              const _val = gameBoard[_node[1]][_node[0]];
 
-            return _val < 1 || _val === endVal;
-          })
-          .map((_node) => ({ pos: _node, prev: _pathKey, prevList: [...path.prevList, path.pos] }));
+              // 0이거나 정답인 칸 탐색
+              return _val < 1 || (_node[0] === _end[0] && _node[1] === _end[1]);
+            })
+            .map((_node) => ({ pos: _node, prev: _pathKey, prevList: [...path.prevList, path.pos] }));
 
-        needVisit = [...needVisit, ...targetPos];
+          needVisit = [...needVisit, ...targetPos];
+        }
       }
     }
 
@@ -137,26 +132,34 @@ function ShisenShoViewer() {
   };
 
   useEffect(() => {
-    console.log(startPos, endPos);
     if (startPos && endPos) {
-      const [isSuccess, pathList] = findPath(startPos, endPos);
+      // 방향성 고정
+      const _s = startPos[0] < endPos[0] ? startPos : endPos;
+      const _e = startPos[0] < endPos[0] ? endPos : startPos;
+
+      const [isSuccess, pathList] = findPath(_s, _e);
 
       if (isSuccess) {
-        const _p = Object.keys(pathList)
-          .reverse()
-          .reduce((acc, cur) => {
-            if (acc.length < 1) {
-              return [pathList[cur].prev, cur];
-            }
-
-            if (acc.includes(cur) && pathList[cur].prev) {
-              return [pathList[cur].prev, ...acc];
-            }
-
-            return acc;
-          }, [] as string[]);
+        const _p = [...pathList[_e.join()].prevList, _e].map((_pos) => _pos.join());
 
         setPath(_p);
+
+        setTimeout(() => {
+          setGameBoard((prev) =>
+            prev.map((_line, _y) =>
+              _line.map((_val, _x) => {
+                if ((_s[0] === _x && _s[1] === _y) || (_e[0] === _x && _e[1] === _y)) {
+                  return 0;
+                } else {
+                  return _val;
+                }
+              })
+            )
+          );
+          setPath([]);
+          setStartPos(null);
+          setEndPos(null);
+        }, 200);
       } else {
         setStartPos(null);
         setEndPos(null);
@@ -164,8 +167,24 @@ function ShisenShoViewer() {
     }
   }, [startPos, endPos]);
 
+  useEffect(() => {
+    setGameBoard(originBoard);
+    setShuffleCount(3);
+  }, [originBoard]);
+
+  useEffect(() => {
+    const remainingScore = gameBoard.flat().reduce((acc, cur) => acc + cur, 0);
+
+    if (remainingScore < 1) {
+      alert('clear!');
+    }
+  }, [gameBoard]);
+
   const handleSetPos = (_pos: ItemPos) => {
-    console.log('??');
+    if (gameBoard[_pos[1]][_pos[0]] < 1) {
+      return;
+    }
+
     if (!startPos && !endPos) {
       setStartPos(_pos);
     } else if (startPos && !endPos && !_.isEqual(_pos, startPos)) {
@@ -173,6 +192,20 @@ function ShisenShoViewer() {
     } else {
       // 같은 칸 클릭의 경우
     }
+  };
+
+  const handleShuffle = () => {
+    if (shuffleCount < 1) {
+      return;
+    }
+
+    const shuffledValues = _.shuffle(gameBoard.flat().filter((_v) => _v));
+
+    const newBoard = gameBoard.map((_line) => _line.map((_v) => (_v > 0 ? shuffledValues.shift() : 0)));
+
+    setGameBoard(newBoard);
+
+    setShuffleCount((prev) => --prev);
   };
 
   return (
@@ -187,12 +220,19 @@ function ShisenShoViewer() {
         >
           {gameBoard.map((_line, _y) =>
             _line.map((_val, _x) => (
-              <div key={`${_x}-${_y}-${_val}`} onClick={() => handleSetPos([_x, _y])}>
-                <b>{_val}</b>
-                <br />
+              <div
+                key={`${_x}-${_y}-${_val}`}
+                onClick={() => handleSetPos([_x, _y])}
+                className={cx({
+                  'has-value': _val > 0,
+                  active: _.isEqual([_x, _y], startPos) || _.isEqual([_x, _y], endPos),
+                })}
+              >
+                <b>{_val > 0 ? _val : ''}</b>
+                {/* <br />
                 <span>
                   {_x},{_y}
-                </span>
+                </span> */}
               </div>
             ))
           )}
@@ -200,6 +240,10 @@ function ShisenShoViewer() {
         <svg className={cx('line-inner')}>
           <polyline points={polylinePoints} />
         </svg>
+      </div>
+
+      <div>
+        <button onClick={handleShuffle}>shuffle.. {`${shuffleCount}/3`}</button>
       </div>
     </div>
   );
